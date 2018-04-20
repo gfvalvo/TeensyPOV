c5df049c3e7d91056655046efe7825b569c9026f
Greg Valvo <gfvalvo@gmail.com>
2018-04-19 21:58:22 -0400

    Update Examples

---
+++ TeensyPOV/examples/SimpleBitmap/SimpleBitmap.h
@@ -0,0 +1,6 @@
+#ifndef _SIMPLE_BITMAP_H_
+#define _SIMPLE_BITMAP_H_
+
+#define NUM_LEDS 36
+
+#endif
---
+++ TeensyPOV/examples/SimpleBitmap/SimpleBitmap.ino
@@ -0,0 +1,26 @@
+#include "TeensyPOV.h"
+#include "SimpleBitmap.h"
+
+extern const LedArrayStruct colorCrossStruct;
+
+const uint8_t clockPin = 13;
+const uint8_t dataPin = 11;
+const uint8_t hallPin = 3;
+const uint32_t numLeds = NUM_LEDS;
+
+CRGB leds[numLeds];
+TeensyPOV display;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Starting POV - Test 1");
+  FastLED.addLeds<APA102, dataPin, clockPin, BGR, DATA_RATE_MHZ(24)>(leds,
+      numLeds);
+  TeensyPOV::povSetup(hallPin, leds, numLeds);
+  display.load(&colorCrossStruct);
+  display.activate();
+}
+
+void loop() {}
