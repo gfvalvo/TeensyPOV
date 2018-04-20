adbde2824e430e3580e46c7e88cdbdbd3ca276d2
Greg Valvo <gfvalvo@gmail.com>
2018-04-20 09:25:00 -0400

    Updated Examples

---
+++ examples/SimpleText/SimpleText.ino
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include "TeensyPOV.h"
+
+#define NUM_LEDS 36
+
+extern const LedArrayStruct pictureStruct;
+
+void switchDisplay(TeensyPOV *);
+void loadRose(TeensyPOV *);
+void loadLimacons(TeensyPOV *);
+void startRpmUpdateTimer(TeensyPOV *);
+void updateRpm(TeensyPOV *);
+
+const uint8_t clockPin = 13;
+const uint8_t dataPin = 11;
+const uint8_t hallPin = 3;
+const uint8_t numColorBits = 2;
+uint8_t tdcSegment = 0;
+const uint16_t numSegements = 128;
+const uint16_t numLeds = NUM_LEDS;
+
+CRGB leds[numLeds];
+
+const uint32_t palette[] = { CRGB::Black, CRGB::Red, CRGB::Green, CRGB::Blue};
+
+TeensyPOV display;
+
+const DisplayStringSpec stringArray[] = { { "HELLO", TOP, 35, 1, 0,
+		false }, { "WORLD", BOTTOM, 35, 3, 0, true } };
+const uint8_t numStrings = sizeof(stringArray) / sizeof(DisplayStringSpec);
+
+void setup() {
+	Serial.begin(115200);
+	delay(1000);
+
+	Serial.println("Starting POV - Test 1");
+	FastLED.addLeds<APA102, dataPin, clockPin, BGR, DATA_RATE_MHZ(24)>(leds,
+			numLeds);
+
+	TeensyPOV::povSetup(hallPin, leds, numLeds);
+
+	display.load(stringArray, numStrings);
+	display.setDisplay(numSegements, numColorBits, tdcSegment, palette);
+
+	while (!TeensyPOV::rpmGood()) {
+	}
+	display.activate();
+}
+
+void loop() {}
+
